312cd07c5dfac2882934df5bb0d600ca4bc8cdc0
Mujibullah Inami <mujibullahinami26@gmail.com>
2023-11-08 02:32:14 +0530

    fix

--- script.js
+++ script.js
@@ -8,8 +8,24 @@ document.addEventListener("DOMContentLoaded", function() {
         lineNumbers: true,
         //closing brackets
         autoCloseBrackets: true,
+
     });
+    //height width
     var width = window.innerWidth;
     editor.setSize(0.7 * width, '500');
+
+    //syntax highlighting according to language
+    var option = document.getElementById('specificSizeSelect');
+    option.addEventListener('change', function() {
+        if(option.value == 'Java'){
+            editor.setOption('mode', 'text/x-java');
+        }
+        else if(option.value == 'python'){
+            editor.setOption('mode', 'text/x-python');
+        }
+        else{
+            editor.setOption('mode', 'text/x-c++src');
+        }
+    })
 });
 
